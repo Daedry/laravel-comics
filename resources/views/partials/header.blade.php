497c7e9f8d3aae4a9fd92d63f974cb2d954c99f3
MyUsername <romamargine@gmail.com>
2022-06-10 16:32:05 +0200

    Add fix header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,83 +1,94 @@
 <header id="site_header">
     <nav>
-
-        <div class="nav-left">
-            @include('partials.logo')
-        </div>
-
-        <div class="nav-right">
-            <div class="links">
-                <ul>
-
-                    <li>
-                        <a href="#">
-                            characters
-                        </a>
-                    </li>
-
-                    <li>
-                        <a href="#">
-                            comics
-                        </a>
-                    </li>
-
-                    <li>
-                        <a href="#">
-                            movies
-                        </a>
-                    </li>
-
-                    <li>
-                        <a href="#">
-                            tv
-                        </a>
-                    </li>
-
-                    <li>
-                        <a href="#">
-                            games
-                        </a>
-                    </li>
-
-                    <li>
-                        <a href="#">
-                            collectibles
-                        </a>
-                    </li>
-
-                    <li>
-                        <a href="#">
-                            videos
-                        </a>
-                    </li>
-
-                    <li>
-                        <a href="#">
-                            fans
-                        </a>
-                    </li>
-
-                    <li>
-                        <a href="#">
-                            news
-                        </a>
-                    </li>
-
-                    <li>
-                        <a href="#">
-                            shop
-                        </a>
-                        <i class="fa-solid fa-caret-down"></i>
-                    </li>
-
-                </ul>
-            </div>
-
-            <div class="search-box">
-                <input type="search" placeholder="Search "><i class='fa-solid fa-magnifying-glass'></i>
-
+        <div class="nav-top">
+            <div class="nav-text">
+                <p class="txt-left">dc power visa&COPY;</p>
+                <p>additional dc site  <i class="fa-solid fa-caret-down"></i></p>
             </div>
         </div>
+        <div class="nav-bot">
+            <div class="nav-left">
+                <!-- @include('partials.logo') -->
+                <div class="logo">
+                    <a href="{{route('homepage')}}"><img src="{{asset('img/dc-logo.png')}}" alt=""></a>
+                </div>
+            </div>
+
+            <div class="nav-right">
+                <div class="links">
+                    <ul>
+
+                        <li>
+                            <a href="{{route('characters')}}">
+                                characters
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="{{route('comics')}}">
+                                comics
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="{{route('movies')}}">
+                                movies
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="{{route('tv')}}">
+                                tv
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="{{route('games')}}">
+                                games
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="{{route('collectibles')}}">
+                                collectibles
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="{{route('videos')}}">
+                                videos
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="{{route('fans')}}">
+                                fans
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="{{route('news')}}">
+                                news
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="{{route('shop')}}">
+                                shop
+                            </a>
+                            <i class="fa-solid fa-caret-down"></i>
+                        </li>
+
+                    </ul>
+                </div>
+
+                <div class="search-box">
+                    <input type="search" placeholder="Search "><i class='fa-solid fa-magnifying-glass'></i>
+
+                </div>
+            </div>
+        </div>
+
 
     </nav>
 </header>
